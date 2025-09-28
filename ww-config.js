8d8a6a5ce9fe693b69f342923bd1a922e48d578e
xequelab <xequelab@gmail.com>
2025-09-28 17:41:44 -0300

    Create ww-config.js

---
+++ ww-config.js
@@ -0,0 +1,23 @@
+export default {
+  editor: {
+    label: {
+      en: "Text Input",
+    },
+  },
+  properties: {
+    placeholder: {
+      label: {
+        en: "Placeholder",
+      },
+      type: "Text",
+      defaultValue: "Type something...",
+    },
+    textColor: {
+      label: {
+        en: "Text color",
+      },
+      type: "Color",
+      defaultValue: "#000000",
+    },
+  },
+};
