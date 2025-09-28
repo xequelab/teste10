43be3b895207ca0cccc64b882f9fb15ea8a8f73c
xequelab <xequelab@gmail.com>
2025-09-28 18:36:28 -0300

    Update wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -2,63 +2,95 @@
   <div class="timepicker-container" :style="containerStyle">
     <input
       type="time"
-      v-model="time"
+      :value="time.value"
       :style="inputStyle"
-      @input="updateValue"
+      @input="onInput($event)"
     />
   </div>
 </template>
 
 <script>
+import { ref, computed, watch, onMounted } from "vue";
+
 export default {
   props: {
-    content: { type: Object, required: true }
+    content: { type: Object, required: true },
+    uid: { type: String, required: true },
+    wwElementState: { type: Object, required: true },
+    /* wwEditor:start */
+    wwFrontState: { type: Object, required: true },
+    wwEditorState: { type: Object, required: true },
+    /* wwEditor:end */
   },
-  data() {
-    return {
-      // Inicializa com content.value para refletir estado anterior
-      time: this.content.value || ""
-    };
-  },
-  computed: {
-    inputStyle() {
-      return {
-        color: this.content.textColor,
-        backgroundColor: this.content.backgroundColor,
-        border: `1px solid ${this.content.borderColor}`,
-        borderRadius: this.content.borderRadius + "px",
-        padding: "8px 12px",
-        fontSize: "16px",
-        outline: "none",
-        cursor: "pointer",
-        width: "150px",
-        boxSizing: "border-box"
-      };
-    },
-    containerStyle() {
-      return {
-        display: "inline-block"
-      };
+  setup(props, { emit }) {
+    const isEditing = computed(() => {
+      /* wwEditor:start */
+      return props.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+      /* wwEditor:end */
+      return false;
+    });
+
+    // Cria variável do componente para o valor do time picker
+    const { value: time, setValue: setTime } = wwLib.wwVariable.useComponentVariable({
+      uid: props.uid,
+      name: "value",
+      type: "string",
+      defaultValue: props.content.value || ""
+    });
+
+    // Estilo do input
+    const inputStyle = computed(() => ({
+      color: props.content.textColor,
+      backgroundColor: props.content.backgroundColor,
+      border: `1px solid ${props.content.borderColor}`,
+      borderRadius: props.content.borderRadius + "px",
+      padding: "8px 12px",
+      fontSize: "16px",
+      outline: "none",
+      cursor: "pointer",
+      width: "150px",
+      boxSizing: "border-box"
+    }));
+
+    const containerStyle = computed(() => ({
+      display: "inline-block"
+    }));
+
+    // Atualiza a variável do componente quando o usuário muda o valor
+    function onInput(event) {
+      setTime(event.target.value);
+      emit("trigger-event", { name: "change", event: { value: event.target.value } });
     }
-  },
-  methods: {
-    updateValue() {
-      // Envia o valor selecionado para o WeWeb
-      this.$emit('update:content', { ...this.content, value: this.time });
-    }
+
+    // Sincroniza valor inicial do content
+    watch(() => props.content.value, (newVal) => {
+      if (newVal !== time.value) {
+        setTime(newVal);
+      }
+    });
+
+    // Opcional: foco automático quando não estiver editando
+    onMounted(() => {
+      if (props.content.autoFocus && !isEditing.value) {
+        nextTick(() => {
+          const input = document.querySelector(`.timepicker-container input`);
+          if (input) input.focus();
+        });
+      }
+    });
+
+    return { time, inputStyle, containerStyle, onInput };
   }
 };
 </script>
 
 <style scoped>
-/* Removendo botões padrão do input time */
 .timepicker-container input::-webkit-inner-spin-button,
 .timepicker-container input::-webkit-clear-button {
   -webkit-appearance: none;
   margin: 0;
 }
 
-/* Foco moderno */
 .timepicker-container input:focus {
   box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
 }
