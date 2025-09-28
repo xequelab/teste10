da7e475d2a5f41365cf9b0d389fb1dd61acfb33b
xequelab <xequelab@gmail.com>
2025-09-28 17:42:03 -0300

    Create wwElement.vue

---
+++ src/wwElement.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input
+      type="text"
+      :placeholder="content.placeholder"
+      :style="inputStyle"
+      v-model="inputValue"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  computed: {
+    inputStyle() {
+      return {
+        color: this.content.textColor,
+        padding: "8px",
+        border: "1px solid #ccc",
+        borderRadius: "6px",
+      };
+    },
+  },
+};
+</script>
